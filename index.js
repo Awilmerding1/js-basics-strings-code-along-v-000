e3e6f807940e6e3fff61532fe4f3d260715c33ed
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-23 16:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const currentUser = "Annabel";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = '';
